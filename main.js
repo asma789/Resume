349c5a17a0a1fe0b17aa51347b244b8f0d2f5e17
unknown <asmathunisashaik@gmail.com>
2018-07-07 16:54:15 +0530

    resume form created

--- main.js
+++ main.js
@@ -13,24 +13,58 @@ LoadJson("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
   careerobj(data.co);
+  education(data.edu);
+  technicalskills(data.tech);
   //console.log(data.co);
-})
+});
 var right=document.getElementById('right');
-var career=document.createElement('h4');
-career.textContent="Career Object :";
-career.appendChild(document.createElement("HR"));
-function careerobj(asma) {
-  var cop=document.createElement("P");
-  cop.textContent=asma;
-  career.appendChild(cop);
+function careerobj(career) {
+var careerobj=document.createElement('h4');
+careerobj.textContent="Career Object :";
+careerobj.appendChild(document.createElement("hr"));
+  var cop=document.createElement("p");
+  cop.textContent=career;
+  right.appendChild(careerobj);
+  right.appendChild(cop);
 }
-right.appendChild(career);
 
 //Education Qualifications
-
-var edu=document.createElement("H4");
-edu.textContent="Educational Qualifications :";
-edu.appendChild(document.createElement("HR"));
-
-
-right.appendChild(edu);
+function education(edu){
+var educational=document.createElement("h4");
+educational.textContent="Educational Qualifications :";
+educational.appendChild(document.createElement("hr"));
+right.appendChild(educational);
+var para1=document.createElement("p");
+var li="";
+for(i in edu){
+  li+=edu[i].name+edu[i].institute+"<br>";
+}
+para1.innerHTML=li;
+right.appendChild(para1);
+}
+function technicalskills(tec){
+  var technical=document.createElement("h4");
+  technical.textcontent="Technical skills :";
+  technical.appendChild(document.createElement("hr"));
+  right.appendChild(technical);
+  var para2=document.createElement("p");
+  var l2="";
+  for(j in tec){
+    l2+=tec[j].name+tec[j].value+"<br>";
+  }
+  para2.innerHTML=l2;
+  right.appendChild(para2);
+}
+function achievements(achieve){
+  var ach=document.createElement("h4");
+  ach.textcontent="Achievements :";
+  ach.appendChild(document.createElement("hr"));
+  right.appendChild(ach);
+  var para3=document.createElement("p");
+  var l3="";
+  for(k in ach){
+    l3+=ach[k].name+ach[k].details+"<br>";
+  }
+  para3.innerHTML=l3;
+  right.appendChild(para3);
+}
